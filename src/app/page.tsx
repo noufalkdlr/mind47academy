2f10b56f73a30d73a32ff9ae0932f3f68d2206d1
Noufal <noufalkakdlr@gmail.com>
2025-12-18 23:21:09 +0530

    create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        Main section
-      </main>
+    <div className="">
+      <div className="px-4 pt-26 bg-black text-white h-dvh">
+        <div className="bg-amber-300 relative overflow-hidden w- h-48 rounded-xl group">
+          <Image
+            src="/images/hero-image-1.jpeg"
+            alt="home-image"
+            width={900} height={200}
+            className="transition-transform scale-120 translate-y-4 duration-500 ease-in-out group-hover:scale-130"
+          />
+        </div>
+        <div>
+          <h1>A Complete mind solution</h1>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,63 @@
-export default function Header(){
-    return(
-        <div>header</div>
-    )
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="fixed w-full z-50">
+      <div className="flex justify-between bg-black px-4 h-16 items-center py-10 ">
+        {/* Logo */}
+
+        <Link href="/">
+          <Image src="/images/logo.png" alt="logo" width={150} height={20} />
+        </Link>
+
+        {/* Desktop menu */}
+
+        <nav className="hidden">desktop menu</nav>
+
+        {/* Hamburger */}
+
+        <button
+          className="text-white flex flex-col gap-1.5 justify-center items-center  w-8"
+          onClick={() => setOpen(!open)}
+        >
+          <span
+            className={`bg-white w-8 h-0.5 transform transition duration-300 ${
+              open ? "rotate-45 translate-y-1" : ""
+            }`}
+          ></span>
+          <span
+            className={`bg-white w-8 h-0.5 transform transition duration-300 ${
+              open ? "-rotate-45 -translate-y-1" : ""
+            }`}
+          ></span>
+        </button>
+      </div>
+      {/* Mobile Menu */}
+
+        <nav
+className={`bg-black text-white flex justify-center transition-all duration-300 ease-in-out  overflow-hidden ${open ? 'max-h-screen opacity-100' : ' max-h-0 opacity-0'}`}
+>
+          <ul className=" my-14 flex flex-col gap-14 text-center">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">About</Link>
+            </li>
+            <li>
+              <Link href="/ffdg">Course</Link>
+            </li>
+            <li>
+              <Link href="/">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+    </header>
+  );
 }
